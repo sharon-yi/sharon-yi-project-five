05cb77c7a90654fc8ed88bfe20a3a910b14f34e5
Sharon <sharon.yi@me.com>
2019-12-03 02:50:32 -0500

    Updates

---
+++ precache-manifest.f36787e7c3e5c86de696f51e0f1790b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f142cb3ac604b9e7cad126b54a76629",
+    "url": "/sharon-yi-project-five/index.html"
+  },
+  {
+    "revision": "fd8e1d9bf983ca24a41c",
+    "url": "/sharon-yi-project-five/static/css/main.41c19413.chunk.css"
+  },
+  {
+    "revision": "4b422b9f13f379928731",
+    "url": "/sharon-yi-project-five/static/js/2.744f93ba.chunk.js"
+  },
+  {
+    "revision": "fd8e1d9bf983ca24a41c",
+    "url": "/sharon-yi-project-five/static/js/main.695c024f.chunk.js"
+  },
+  {
+    "revision": "d898f55b42b4d5b01831",
+    "url": "/sharon-yi-project-five/static/js/runtime-main.c53a913c.js"
+  },
+  {
+    "revision": "2cace33266c021a91a0a4e8a785325d8",
+    "url": "/sharon-yi-project-five/static/media/backgroundImage.2cace332.svg"
+  }
+]);
